29712d89ccddb7f2e375cb55565ff4f9af674585
rodolfoizidoro <rodolfoizidoro@gmail.com>
2017-03-02 13:59:11 -0300

    Gerador numeros

--- Gerador Numeros/ViewController.swift
+++ Gerador Numeros/ViewController.swift
@@ -14,6 +14,14 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
+    @IBOutlet weak var legendaResultado: UILabel!
+    @IBAction func gerarNumero(_ sender: Any) {
+        
+        let numeroAleatorio = arc4random_uniform( 101 );
+        
+        legendaResultado.text = String(numeroAleatorio);
+        
+    }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
